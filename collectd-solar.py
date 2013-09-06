6a1fe4cf93217ab66eb0cea22d1bc4e4d1b24ac0
Aaron Huslage <aaron@iilab.org>
2013-09-05 20:42:23 -0400

    output proper data in the right format for collectd

--- collectd-solar.py
+++ collectd-solar.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python 
+
 import minimalmodbus
 import os
 
@@ -11,26 +13,25 @@ hostname = os.getenv('COLLECTD_HOSTNAME')
 
 data =  sunsaver.read_registers(8,45)
 
-for (i, j) in enumerate(data):
-    print i, j
+iLoad=data[4]*79.16/32768.0
+vLoad=data[2]*100.0/32768.0
+AhLoad=((data[23] << 16) + data[24])*0.1
+print "PUTVAL \"%s/sunsaver/solar_load\" interval=%s N:%0.2f N:%0.2f N:%0.2f" % (hostname, interval, iLoad, vLoad, AhLoad)
 
-battVoltage=data[0]*100.0/32768.0
-arrayVoltage=data[1]*100.0/32768.0
-loadVoltage=data[2]*100.0/32768.0
-chargeCurrent=data[3]*100.0/32768.0
-loadCurrent=data[4]*79.16/32768.0
-ambientTemp=data[7]
-powerOut=data[31]*989.5/65536.0
+vArray=data[1]*100.0/32768.0
+vMP=data[32]*100.0/32768.0
+pMax=data[33]*989.5/65536.0
+vOC=data[34]*100.0/32768.0
+print "PUTVAL \"%s/sunsaver/solar_array\" interval=%s N:%0.2f N:%0.2f N:%0.2f N:%0.2f" % (hostname, interval, vArray, vMP, pMax, vOC)
 
+vBatt=data[0]*100.0/32768.0
+print "PUTVAL \"%s/sunsaver/solar_battery\" interval=%s N:%0.2f" % (hostname, interval, vBatt)
 
-def collectdPrint( name, var ):
-	print "PUTVAL \"%s/sunsaver/%s\" interval=%s N:%0.2f" % (hostname, name, interval, var)
+iCharge=data[3]*100.0/32768.0
+AhCharge=((data[13] << 16) + data[14])*0.1
+kWhCharge=data[17]*0.1
+WCharge=data[31]*989.5/65536.0
+print "PUTVAL \"%s/sunsaver/solar_charge\" interval=%s N:%0.2f N:%0.2f N:%0.2f N:%0.2f" % (hostname, interval, iCharge, AhCharge, kWhCharge, WCharge)
 
-collectdPrint( "battVoltage", battVoltage )
-collectdPrint( "arrayVoltage", arrayVoltage )
-collectdPrint( "loadVoltage", loadVoltage )
-collectdPrint( "chargeCurrent", chargeCurrent )
-collectdPrint( "loadCurrent", loadCurrent )
-collectdPrint( "powerOut", powerOut )
-collectdPrint( "ambientTemp", ambientTemp )
-
+ambientC=data[7]
+print "PUTVAL \"%s/sunsaver/solar_temp\" interval=%s N:%0.2f" % (hostname, interval, ambientC)
