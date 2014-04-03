045f52b5b24d0799958eb41c06aa1878c8da090a
Jun Matsushita <jun@iilab.org>
2014-04-03 12:36:52 +0100

    Fixed exception handling and added timer

--- solar.py
+++ solar.py
@@ -2,19 +2,23 @@
 
 import minimalmodbus
 import os
+import time
+
+from serial import SerialException
 
 while True:
+        time.sleep(0.5)
         try:
                 sunsaver = minimalmodbus.Instrument( '/dev/ttyS1', 1 )
 
                 sunsaver.serial.baudrate = 9600
                 sunsaver.serial.stopbits = 2
-        
+
                 interval = os.getenv('COLLECTD_INTERVAL')
                 hostname = os.getenv('COLLECTD_HOSTNAME')
-        
+
                 data = sunsaver.read_registers(8,45)
-        
+
                 battVoltage=data[0]*100.0/32768.0
                 loadVoltage=data[2]*100.0/32768.0
                 arrayVoltage=data[1]*100.0/32768.0
@@ -22,8 +26,8 @@ while True:
                 loadCurrent=data[4]*79.16/32768.0
                 ambientTemp=data[7]
                 powerOut=data[31]*989.5/65536.0
-        
+
                 print "battVoltage: %1.2fV / arrayVoltage: %1.2fV / loadVoltage: %1.2fV / chargeCurrent: %1.2fA / loadCurrent: %1.2fA / powerOut: %1.2fW / ambientTemp: %1.2fC" % (battVoltage, arrayVoltage, loadVoltage, chargeCurrent, loadCurrent, powerOut, ambientTemp)
 
-        except serial.serialutil.SerialException:
+        except SerialException:
                 pass
