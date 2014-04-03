16aef528adbf1ef1e21736040d7407c34b5927c8
Jun Matsushita <jun@iilab.org>
2014-04-03 12:16:37 +0100

    Update solar.py

--- solar.py
+++ solar.py
@@ -3,25 +3,26 @@
 import minimalmodbus
 import os
 
-sunsaver = minimalmodbus.Instrument( '/dev/ttyS1', 1 )
+import minimalmodbus
+import os
 
-sunsaver.serial.baudrate = 9600
-sunsaver.serial.stopbits = 2
+while True:
+        sunsaver = minimalmodbus.Instrument( '/dev/ttyS1', 1 )
 
-interval = os.getenv('COLLECTD_INTERVAL')
-hostname = os.getenv('COLLECTD_HOSTNAME')
+        sunsaver.serial.baudrate = 9600
+        sunsaver.serial.stopbits = 2
 
-data = sunsaver.read_registers(8,45)
+        interval = os.getenv('COLLECTD_INTERVAL')
+        hostname = os.getenv('COLLECTD_HOSTNAME')
 
-for (i, j) in enumerate(data):
-    print i, j
+        data = sunsaver.read_registers(8,45)
 
-battVoltage=data[0]*100.0/32768.0
-arrayVoltage=data[1]*100.0/32768.0
-loadVoltage=data[2]*100.0/32768.0
-chargeCurrent=data[3]*100.0/32768.0
-loadCurrent=data[4]*79.16/32768.0
-ambientTemp=data[7]
-powerOut=data[31]*989.5/65536.0
+        battVoltage=data[0]*100.0/32768.0
+        loadVoltage=data[2]*100.0/32768.0
+        arrayVoltage=data[1]*100.0/32768.0
+        chargeCurrent=data[3]*100.0/32768.0
+        loadCurrent=data[4]*79.16/32768.0
+        ambientTemp=data[7]
+        powerOut=data[31]*989.5/65536.0
 
-print "battVoltage: %1.2fV / arrayVoltage: %1.2fV / loadVoltage: %1.2fV / chargeCurrent: %1.2fA / loadCurrent: %1.2fA / powerOut: %1.2fW / ambientTemp: %1.2fC" % (battVoltage, arrayVoltage, loadVoltage, chargeCurrent, loadCurrent, powerOut, ambientTemp)
+        print "battVoltage: %1.2fV / arrayVoltage: %1.2fV / loadVoltage: %1.2fV / chargeCurrent: %1.2fA / loadCurrent: %1.2fA / powerOut: %1.2fW / ambientTemp: %1.2fC" % (battVoltage, arrayVoltage, loadVoltage, chargeCurrent, loadCurrent, powerOut, ambientTemp)
